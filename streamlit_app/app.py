1b129e002a361ca7355932c104b71f6129f5bd36
mamabox <fatou@mamafatou.com>
2026-07-31 17:31:17 +0200

    ADD rename pages folder to views

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -8,11 +8,11 @@ st.set_page_config(
 )
 
 # --- Define pages ---
-home = st.Page("pages/home.py", title="HOME", default=True)
-static_map = st.Page("pages/static_map.py", title="Static Map")
-animated_map = st.Page("pages/animated_map.py", title="Animated Map")
-video_sync = st.Page("pages/video_sync.py", title="Replay and Video")
-about = st.Page("pages/about.py", title="About")
+home = st.Page("views/home.py", title="HOME", default=True)
+static_map = st.Page("views/static_map.py", title="Static Map")
+animated_map = st.Page("views/animated_map.py", title="Animated Map")
+video_sync = st.Page("views/video_sync.py", title="Replay and Video")
+about = st.Page("views/about.py", title="About")
 
 # --- Navigation ---
 nav = st.navigation([home, static_map, animated_map, video_sync, about])
